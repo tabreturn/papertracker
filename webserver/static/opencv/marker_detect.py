ea287db4ddf812df24dff2f79b12b01f5f167a74
tabreturn <github@tabreturn.com>
2019-10-13 16:24:12 +1300

    - convert test jpg to png

--- webserver/static/opencv/marker_detect.py
+++ webserver/static/opencv/marker_detect.py
@@ -1,7 +1,7 @@
 import cv2
 import cv2.aruco as aruco
 
-photo = cv2.imread('test.jpg')
+photo = cv2.imread('test.png')
 arucodict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
 params = aruco.DetectorParameters_create()
 corners, ids, rejectedpoints = aruco.detectMarkers(photo, arucodict, parameters=params)
@@ -9,5 +9,5 @@ corners, ids, rejectedpoints = aruco.detectMarkers(photo, arucodict, parameters=
 aruco.drawDetectedMarkers(photo, corners, ids)
 aruco.drawDetectedMarkers(photo, rejectedpoints, borderColor=(100, 0, 240))
 
-cv2.imwrite('result.jpg', photo);
+cv2.imwrite('result.png', photo);
 cv2.waitKey(0);
