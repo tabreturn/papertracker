03cab397e453a8ef68fa7f4e75776d40417915bd
tabreturn <github@tabreturn.com>
2019-11-04 14:28:19 +1300

    create instrument icons (svg)

--- webserver/static/TileConfig.js
+++ webserver/static/TileConfig.js
@@ -8,10 +8,10 @@ export default
 // python config (DetectTiles.py) reads from line 9 -- do not move!
 {
   "arrows": {
-    "N": "↑",
-    "S": "↓",
-    "W": "←",
-    "E": "→"
+    "N": "<img src='./static/samples/N.svg' />",
+    "S": "<img src='./static/samples/S.svg' />",
+    "W": "<img src='./static/samples/W.svg' />",
+    "E": "<img src='./static/samples/E.svg' />"
   },
 
   "colors": {
@@ -22,10 +22,10 @@ export default
   },
 
   "instruments": {
-    "A_hat": [20, "ѻ", ["sample", "./static/samples/hihat.mp3"] ],
-    "A_kick1": [21, "Ô", ["sample", "./static/samples/kick1.mp3"] ],
-    "A_kick2": [22, "Ö", ["sample", "./static/samples/kick2.mp3"] ],
-    "A_stab1": [23, "🎹",["synth", {
+    "A_hat":   [20, "<img src='./static/samples/hihat.svg' />",  ["sample", "./static/samples/hihat.mp3"] ],
+    "A_kick1": [21, "<img src='./static/samples/kick1.svg' />",  ["sample", "./static/samples/kick1.mp3"] ],
+    "A_kick2": [22, "<img src='./static/samples/kick2.svg' />",  ["sample", "./static/samples/kick2.mp3"] ],
+    "A_stab1": [23, "<img src='./static/samples/synth.svg' />",  ["synth", {
         "oscillator" : {"type":"amtriangle", "harmonicity":0.5, "modulationType":"sine"},
         "envelope"   : {"attackCurve":"exponential", "attack":0.05, "decay":0.2, "sustain":0.2, "release":1.5},
         "portamento" : 0.05	},
